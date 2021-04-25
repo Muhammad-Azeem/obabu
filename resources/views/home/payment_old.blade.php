81b7c5d7399fa01bac859271dcea914b7691f664
umershahidcp <umershahidcp@gmail.com>
2021-04-25 23:10:43 +0500

    payments

--- resources/views/home/payment_old.blade.php
+++ resources/views/home/payment_old.blade.php
@@ -3,7 +3,7 @@
 @section('maincontent')
     <section class="payment_cards">
         <div class="container-fluid">
-            <div class="card_main yellow_card">
+            <div class="card_main">
                 <div class="card_title">
                     <h3>Personal Detail</h3>
                 </div>
@@ -13,47 +13,46 @@
                     <input type="hidden" name="package_id" value="{{$data['package']->id}}">
                     <input type="hidden" name="type" value="{{$data['type']}}">
                     <input type="hidden" name="option_id" value="{{isset($data['option']->id)?$data['option']->id:null}}">
-
-                    <div class="card_fields">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="custom_input green_inp">
-                                    <input type="text" name="name" onkeyup="replaceName()" placeholder="Full Name" class="form-control get_name" required>
-                                </div>
-                            </div>
-                            {{--                        <div class="col-md-6">--}}
-                            {{--                            <div class="custom_input green_inp">--}}
-                            {{--                                <input type="text" name="" placeholder="Last Name" class="form-control">--}}
-                            {{--                            </div>--}}
-                            {{--                        </div>--}}
-                            <div class="col-md-12">
-                                <div class="custom_input green_inp">
-                                    <input type="email" name="email" placeholder="Email Address" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="custom_input green_inp">
-                                    <input type="number" name="phone" placeholder="Phone Number" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="custom_input green_inp">
-                                    <input type="password" name="password" placeholder="Password" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="payment_top_cartoon">
-                                <img src="{{asset('public/assets/images/blue_cartoon.png')}}" class="img-fluid">
-                            </div>
-                            <div class="payment_bottom_cartoon">
-                                <img src="{{asset('public/assets/images/info-box-after.png')}}" class="img-fluid">
+                <div class="card_fields">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="custom_input green_inp">
+                                <input type="text" name="name" onkeyup="replaceName()" placeholder="Full Name" class="form-control get_name" required>
                             </div>
                         </div>
+{{--                        <div class="col-md-6">--}}
+{{--                            <div class="custom_input green_inp">--}}
+{{--                                <input type="text" name="" placeholder="Last Name" class="form-control">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+                        <div class="col-md-12">
+                            <div class="custom_input green_inp">
+                                <input type="email" name="email" placeholder="Email Address" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="custom_input green_inp">
+                                <input type="number" name="phone" placeholder="Phone Number" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="custom_input green_inp">
+                                <input type="password" name="password" placeholder="Password" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="payment_top_cartoon">
+                            <img src="{{asset('public/assets/images/blue_cartoon.png')}}" class="img-fluid">
+                        </div>
+                        <div class="payment_bottom_cartoon">
+                            <img src="{{asset('public/assets/images/info-box-after.png')}}" class="img-fluid">
+                        </div>
+                    </div>
                     <div class="radio_img">
                         <img src="{{asset('public/assets/images/red_radio.png')}}">
                     </div>
                 </div>
             </div>
-            <div class="card_main pink_card">
+            <div class="card_main chocolate">
                 <div class="card_title">
                     <h3>Payment Detail</h3>
                 </div>
@@ -94,22 +93,23 @@
                                     <h4>Accepted payments</h4>
                                 </div>
                                 <div class="accept_imgs">
-                                    {{--                                    <img src="assets/images/Icon awesome-paypal.png">--}}
+{{--                                    <img src="assets/images/Icon awesome-paypal.png">--}}
                                     <img src="{{asset('public/assets/images/Icon awesome-cc-visa.png')}}">
                                     <img src="{{asset('public/assets/images/Icon awesome-credit-card.png')}}">
                                 </div>
                             </div>
                         </div>
-                        {{--                        <div class="col-md-6">--}}
-                        {{--                            <div class="custom_input green_inp">--}}
-                        {{--                                <input type="number" name="" placeholder="Total Amount " class="form-control">--}}
-                        {{--                            </div>--}}
-                        {{--                        </div>--}}
+{{--                        <div class="col-md-6">--}}
+{{--                            <div class="custom_input green_inp">--}}
+{{--                                <input type="number" name="" placeholder="Total Amount " class="form-control">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
                     </div>
                     <div class="checkout_btn">
-                        <button type="button" class="btn">CHECKOUT</button>
+                       <button type="submit" class="btn">CHECKOUT</button>
                     </div>
                 </div>
+                </form>
                 <div class="radio_img">
                     <img src="{{asset('public/assets/images/yellow_radio.png')}}">
                 </div>
@@ -125,17 +125,17 @@
                                 <h5>User Name : </h5>
                                 <p class="user_name"></p>
                             </div>
-                            {{--                            <div class="info_titles">--}}
-                            {{--                                <h5>Order Number :</h5>--}}
-                            {{--                                <p>1236k54</p>--}}
-                            {{--                            </div>--}}
+{{--                            <div class="info_titles">--}}
+{{--                                <h5>Order Number :</h5>--}}
+{{--                                <p>1236k54</p>--}}
+{{--                            </div>--}}
                             <div class="info_titles">
                                 <h5>Package Name : </h5>
                                 <p>{{strtoupper($data['package']->name)}} <span class="blur_color">Per {{ucfirst(str_replace('ly','',$data['type']))}}</span></p>
                             </div>
-                            {{--                            <div class="manage_order">--}}
-                            {{--                                <button type="button" class="btn">View or Manage Order</button>--}}
-                            {{--                            </div>--}}
+{{--                            <div class="manage_order">--}}
+{{--                                <button type="button" class="btn">View or Manage Order</button>--}}
+{{--                            </div>--}}
                         </div>
                         <div class="col-md-6">
                             <div class="info_titles mb-3">
@@ -166,7 +166,6 @@
             <hr>
         </div>
     </section>
-
     <script>
         Stripe.setPublishableKey('{{ env('STRIPE_KEY')}}');
         jQuery(function ($) {
