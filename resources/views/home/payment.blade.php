6d0495a1f3db7c024d0b00e2b65d2853f2ea39d8
umair-moghal <moonumair92@gmail.com>
2021-04-27 02:11:54 -0700

    new pages header set add slider

--- resources/views/home/payment.blade.php
+++ resources/views/home/payment.blade.php
@@ -1,6 +1,73 @@
 @extends('layouts.main')
 @section('title','Obabu Home')
 @section('maincontent')
+<header class="header ot_headers">
+        <nav class="navbar navbar-light navbar-expand-lg bg-transparent">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/images/logo.png" class="img-fluid mx-auto" height="114" width="139" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                            @php
+            $segment = \Illuminate\Support\Facades\Request::segment(1);
+            @endphp
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item {{($segment == '') ? 'active' :''}}">
+                        <a class="nav-link text-white" href="{{url('/')}}">HOME</a>
+                    </li>
+                    <li class="nav-item {{($segment == 'about-us') ? 'active' :''}}">
+                        <a class="nav-link" href="{{route('about_us')}}">ABOUT US</a>
+                    </li>
+                    <li class="nav-item {{($segment == 'sales') ? 'active' :''}}">
+                        <a class="nav-link" href="{{route('sales')}}">SALES</a>
+                    </li>
+                    <li class="nav-item {{($segment == 'membership') ? 'active' :''}}">
+                        <a class="nav-link" href="{{route('membership')}}">MEMBERSHIPS</a>
+                    </li>
+                    <li class="nav-item {{($segment == 'chat_view') ? 'active' :''}}">
+                        <a class="nav-link">CHAT VIEW</a>
+                    </li>
+                    <li class="nav-item">
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </li>
+                </ul>
+                @auth
+                <div class="my-2 my-lg-0">
+                    <img src="{{asset('public/assets/images/profile.png')}}" alt="User Image" class="img-fluid" width="124" height="124"/>
+                </div>
+                @endauth
+            </div>
+            </div>
+        </nav>
+    </header>
     <section class="payment_cards">
         <div class="container-fluid">
             <div class="card_main yellow_card">
